82f5945b6fd2c4400ad11ec0a8d84dea457e1a43
Leonardo Gomes <lgdevlop@gmail.com>
2018-02-23 17:31:48 -0300

    Webpack com Dev-Server toolchain

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    }
+}
