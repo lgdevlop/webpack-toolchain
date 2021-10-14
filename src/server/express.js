9b8abed576528c503341fa9d2e7492ce079e126b
Israel Neves Micha <isrmicha@gmail.com>
2021-10-14 19:54:20 -0300

    chore: refactoring to accept PORT env (#28)

--- src/server/express.js
+++ src/server/express.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
 const server = express()
 const public = express.static('./dist')
 
+const PORT = process.env.PORT || 8080
+
 server.use(public)
 
-server.listen(8080, () => {
+server.listen(PORT, () => {
   console.log('Servidor escutando...')
 })
